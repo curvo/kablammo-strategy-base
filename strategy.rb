5105a15c8ffb5fef9285374714cf53709a036e47
Brian Case <brian@tobi.com>
2013-09-21 13:44:17 -0700

    af07c8e patch

--- strategy.rb
+++ strategy.rb
@@ -6,8 +6,10 @@ extend Defensive
 
 def go!
   enemy = opponents.first
-  return fire_at!(enemy, line_of_site_to(enemy)) if can_fire_at?(enemy)
 
+  return fire_at! enemy if my.can_fire_at? enemy
+  return dodge(enemy) if enemy.can_fire_at?(me)   
+  return aim_at!(enemy) unless aiming_at?(enemy)
   rest
 end
 
