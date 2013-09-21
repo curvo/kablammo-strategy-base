d773419f2d16a4cf36b3136dc88d43fc02d7e544
Brian Case <brian@tobi.com>
2013-09-21 16:59:06 -0700

    more

--- strategy.rb
+++ strategy.rb
@@ -7,10 +7,10 @@ extend Defensive
 def go!
   enemy = opponents.first
   return hunt unless enemy
-  return rest if my.ammo == 0 && !enemy.can_fire_at?(me)
+  return rest if (my.ammo == 0 && !enemy.can_fire_at?(me))
   return move_towards! enemy if obscured?(enemy)
-  return fire_at!(enemy, 1) if (can_fire_at?(enemy) && my.armor > 1)
-  return fire_at!(enemy, 1) if (can_fire_at?(enemy) && !enemy.can_fire_at?(me))
+  return fire_at!(enemy) if can_fire_at?(enemy)
+  return fire_at!(enemy) if (can_fire_at?(enemy) && !enemy.can_fire_at?(me))
   return dodge(enemy) if (enemy.can_fire_at?(me) && my.armor == 1 && can_fire_at?(enemy))
   return fire_at! enemy if can_fire_at?(enemy)
   return aim_at!(enemy) unless aiming_at?(enemy)
