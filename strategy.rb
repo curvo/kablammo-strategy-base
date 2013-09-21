f4956ae7c5584d21fdd448f882bfadeb36b410a2
Brian Case <brian@tobi.com>
2013-09-21 13:51:41 -0700

    added extra dodge

--- strategy.rb
+++ strategy.rb
@@ -7,7 +7,7 @@ extend Defensive
 def go!
   enemy = opponents.first
   return hunt unless enemy            
-
+  return dodge(enemy) if enemy.can_fire_at?(me) && my.armor < 2  
   return fire_at! enemy if my.can_fire_at? enemy
   return dodge(enemy) if enemy.can_fire_at?(me)   
   return aim_at!(enemy) unless aiming_at?(enemy)
