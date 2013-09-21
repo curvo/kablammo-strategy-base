731a08d68af5b5c0c16eb56b871d224a0598f8c9
Brian Case <brian@tobi.com>
2013-09-21 14:38:13 -0700

    more fires

--- strategy.rb
+++ strategy.rb
@@ -6,11 +6,9 @@ extend Defensive
 
 def go!
   enemy = opponents.first
-  return aim_at!(enemy) unless aiming_at?(enemy) && !enemy.can_fire_at?(me) 
+  return aim_at!(enemy) unless aiming_at?(enemy)
   return fire_at!(enemy, 3) if can_fire_at? enemy
   return dodge(enemy) if enemy.can_fire_at?(me)
-  return fire_at! enemy if can_fire_at? enemy
-  return aim_at!(enemy) unless aiming_at?(enemy)
   return hunt unless enemy
 
    # && my.ammo > 0
