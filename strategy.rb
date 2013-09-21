07ab5897aabff7da4fd58b05a9628391d4a68994
Brian Case <brian@tobi.com>
2013-09-21 14:31:39 -0700

    two fires

--- strategy.rb
+++ strategy.rb
@@ -7,9 +7,10 @@ extend Defensive
 def go!
   enemy = opponents.first
   return hunt unless enemy
-  return dodge(enemy) if enemy.can_fire_at?(me)
-  return aim_at!(enemy) unless aiming_at?(enemy)
   return fire_at! enemy if (can_fire_at? enemy && my.armor > 1 )
+  return move_away_from!(enemy) if enemy.can_fire_at?(me)
+  return aim_at!(enemy) unless aiming_at?(enemy)
+  return fire_at! enemy if can_fire_at? enemy
 
    # && my.ammo > 0
   # return rest if my.ammo == 0
